360f742421968a0486d4d097eece36e58c5b40a5
Shreya220 <100401171+Shreya220@users.noreply.github.com>
2022-04-03 13:26:44 +0530

    Create ReverseANumber.cpp

---
+++ ReverseANumber.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int reverse;
+    while (n > 0)
+    {
+        int rem = n % 10;
+        n /= 10;
+        reverse = reverse * 10 + rem;
+    }
+    
+    cout << "Reversed number: " << reverse;
+    return 0;
+}
