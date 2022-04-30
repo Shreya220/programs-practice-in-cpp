4ea421b6b1c6b6c52aab5d9fb039cb81f3f107d7
Shreya220 <100401171+Shreya220@users.noreply.github.com>
2022-04-30 13:10:44 +0530

    Create selection-sort.cpp

---
+++ selection-sort.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the size of array: ";
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[i])
+            {
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+    //                    ** LEARNING**
+    //* j ka loop saare elements ke saath minimum find krne ke liye hai
+    //* j ka loop i ko baaki elements ke saath compare krega isliye i+1 se start hai
+    //* i ka loop n-1 tk isliye chala coz n se ek kam me hi ho jayenge elements sort , last wala is sorted already
+    //*i ka loop ek sorted array create kr rha hai...pehle 0th sort hua then 1st then 2nd------2nd last and last to sorted h already
+    
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
