17880bee5e05751f7a9b171be33bc4a06a822356
Shreya220 <100401171+Shreya220@users.noreply.github.com>
2022-07-18 10:02:11 +0530

    Update bubble-sort.cpp

--- bubble-sort.cpp
+++ bubble-sort.cpp
@@ -32,3 +32,19 @@ int main()
     }
     return 0;
 }
+//ANOTHER METHOD FOR BUBBLE SORT
+void bubbleSort(vector<int>& arr, int n)
+{   
+    for(int i = 1; i < n;i++)
+    {
+        //for round 1 to n-1
+        for(int j = 0; j < n - i; j++)
+        {
+            //process elements till n-i th index
+            if(arr[j] > arr[j+1])
+            {
+                swap(arr[j] , arr[j+1]);
+            }
+        }
+    }
+}
