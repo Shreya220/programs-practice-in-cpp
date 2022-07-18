c9593d0adb1d9e23f9ab5d4d3c9046845dd735a2
Shreya220 <100401171+Shreya220@users.noreply.github.com>
2022-07-18 20:18:58 +0530

    Update insertion-sort.cpp

--- insertion-sort.cpp
+++ insertion-sort.cpp
@@ -37,3 +37,24 @@ int main()
     }
     return 0;
 }
+//ANOTHER METHOD FOR INSERTION SORT
+void insertionSort(int n, vector<int> &arr){
+    for(int i = 1 ; i < n; i++)
+    {
+        int temp = arr[i];
+        int j = i - 1;
+        for(; j >= 0;j--)
+        {
+            if(arr[j] > temp)
+            {
+                //shift
+                arr[j+1] = arr[j];
+            }
+            else
+            {
+                break;
+            }
+        }
+        arr[j+1] = temp;
+    }
+}
